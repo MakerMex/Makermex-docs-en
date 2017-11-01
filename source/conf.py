1eb8927e2a1316d46de389bac8808f4c1e6efc93
jorajipo <jorajipo@gmail.com>
2017-11-01 01:32:38 -0600

    Avance traduccion

--- source/conf.py
+++ source/conf.py
@@ -66,7 +66,7 @@ release = u'1.5'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'es'
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -221,7 +221,7 @@ html_static_path = ['_static']
 #   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr', 'zh'
 #
-# html_search_language = 'es'
+# html_search_language = 'en'
 
 # A dictionary with options for the search language support, empty by default.
 # 'ja' uses this config value.
